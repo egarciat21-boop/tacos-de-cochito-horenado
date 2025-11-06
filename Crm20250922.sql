4b42884f88a68a382bb846547c9c91c62a68568d
egarciat21-boop <egarciat21@miumg.edu.gt>
2025-11-05 18:52:00 -0600

    Add files via upload

--- Crm20250922.sql
+++ Crm20250922.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
--- Host: localhost    Database: crm
+-- Host: localhost    Database: proyecto
 -- ------------------------------------------------------
--- Server version	8.4.6
+-- Server version	8.0.43
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,7 +32,7 @@ CREATE TABLE `cliente` (
   `saldo_pendiente` float DEFAULT NULL,
   `tipo_cliente` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +41,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'Pablo','165156','ciudad','123499','none',0,'frecuente'),(2,'Pedro','121212','12','54656465','65+5',0,'frecuente');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,10 +143,12 @@ CREATE TABLE `inventario` (
   `id_inventario` int NOT NULL AUTO_INCREMENT,
   `id_producto` int DEFAULT NULL,
   `cantidad` int DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
   PRIMARY KEY (`id_inventario`),
   KEY `fk_producto_idx` (`id_producto`) /*!80000 INVISIBLE */,
   CONSTRAINT `fk_productos` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,6 +157,7 @@ CREATE TABLE `inventario` (
 
 LOCK TABLES `inventario` WRITE;
 /*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+INSERT INTO `inventario` VALUES (4,6,1000,'ENTRADA',NULL),(5,6,1,'SALIDA','2025-10-21'),(6,7,500,'ENTRADA',NULL),(7,7,50,'SALIDA','2025-10-21'),(8,6,50,'SALIDA','2025-10-22'),(9,7,50,'SALIDA','2025-10-22'),(10,6,10,'SALIDA','2025-10-23'),(11,6,10,'SALIDA','2025-10-23');
 /*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,8 +176,11 @@ CREATE TABLE `producto` (
   `stock` int DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
   `categoria` varchar(45) DEFAULT NULL,
+  `producto_categoria` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
   PRIMARY KEY (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,6 +189,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` VALUES (6,'Camisas','M',600,929,'Nuevo','HOMBRE','CAMISAS','','2025-10-21'),(7,'Pantalones','L',150,400,'nuevo','NIÑO','PANTALONES','','2025-10-21');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -198,12 +206,14 @@ CREATE TABLE `venta` (
   `total` float DEFAULT NULL,
   `id_cliente` int DEFAULT NULL,
   `id_empleado` int DEFAULT NULL,
+  `comprobante` varchar(45) DEFAULT NULL,
+  `ventas` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_venta`),
   KEY `fk_cliente_idx` (`id_cliente`),
   KEY `fk_empleado_idx` (`id_empleado`),
   CONSTRAINT `fk_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
   CONSTRAINT `fk_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,6 +222,7 @@ CREATE TABLE `venta` (
 
 LOCK TABLES `venta` WRITE;
 /*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+INSERT INTO `venta` VALUES (1,'2025-10-21 22:44:03',672,1,NULL,'V-20251021-000001','SUB=Q600.00|IVA=Q72.00|TOTAL=Q672.00'),(2,'2025-10-21 22:53:14',672,2,NULL,'V-20251021-000002','SUB=Q600.00|IVA=Q72.00|TOTAL=Q672.00'),(3,'2025-10-21 22:58:23',13440,2,NULL,'V-20251021-000003','SUB=Q12000.00|IVA=Q1440.00|TOTAL=Q13440.00'),(4,'2025-10-21 23:10:02',13440,2,NULL,'V-20251021-000004','SUB=Q12000.00|IVA=Q1440.00|TOTAL=Q13440.00'),(5,'2025-10-21 23:19:11',336000,2,NULL,'V-20251021-000005','SUB=Q300000.00|IVA=Q36000.00|TOTAL=Q336000.00'),(6,'2025-10-21 23:26:46',672,2,NULL,'V-20251021-000006','SUB=Q600.00|IVA=Q72.00|TOTAL=Q672.00'),(7,'2025-10-21 23:44:28',8400,1,NULL,'V-20251021-000007','SUB=Q7500.00|IVA=Q900.00|TOTAL=Q8400.00'),(8,'2025-10-22 22:39:54',42000,2,NULL,'V-20251022-000001','SUB=Q37500.00|IVA=Q4500.00|TOTAL=Q42000.00'),(9,'2025-10-23 06:59:08',6720,2,NULL,'V-20251023-000001','SUB=Q6000.00|IVA=Q720.00|TOTAL=Q6720.00'),(10,'2025-10-23 10:30:24',6720,1,NULL,'V-20251023-000002','SUB=Q6000.00|IVA=Q720.00|TOTAL=Q6720.00');
 /*!40000 ALTER TABLE `venta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -224,4 +235,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-22 21:06:22
+-- Dump completed on 2025-11-03 22:38:43
